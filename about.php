d2b1c50bd213bae919b364206450440f44c3b2e5
deftiagita <deftaigita2019@gmail.com>
2024-04-17 06:46:33 +0700

    first commit

---
+++ about.php
@@ -0,0 +1,18 @@
+<?php 
+	include 'header.php';
+ ?>
+
+
+<div class="container" style="padding-bottom: 300px;">
+	<h2 style=" width: 100%; border-bottom: 4px solid #ff8680"><b>About Us</b></h2>
+	<p class="text-justify">Cake Me Out menganggap satu sama lain sebagai keluarga, dan kesuksesan kami adalah karena kami memegang teguh nilai-nilai kekeluargaan. Kami telah memperluas lingkaran keluarga untuk mencakup pelanggan kami. Bagi kami, pelanggan bukanlah seseorang yang sekedar membeli kue dari salahsatu toko kami. Saat kita membuat kue untuk acara spesial seseorang, kita tahu bahwa kita diizinkan masuk ke dalam keluarga orang tersebut melalui kue tersebut. Itulah sebabnya setiap kue dipanggang dan dihias seolah-olah itu diperuntukkan bagi salah satu anggota keluarga kami.</p>
+<p>
+Cake Me Out adalah salah satu bisnis makanan kue yang memiliki 10 cabang di Indonesia. Tersebar diberbagai daerah yaitu, Jakarta, Surabaya, Bekasi, Pati, Semarang, Magelang, Malang, Yogyakarta, Solo, dan Madiun . Perusahaan memulai perjalanannya pada tahun 2003 dengan lokasi yang kecil di dalam rumah. Produk kami sehat, bergizi, dan terjangkau oleh semua orang.</p>
+</div>
+
+
+
+
+ <?php 
+	include 'footer.php';
+ ?>
---
+++ footer.php
@@ -0,0 +1,31 @@
+
+	<footer style="border-top: 4px solid #87CEEB;  ">
+		<div class="container" style="padding-bottom: 50px;">
+			<div class="row">
+				<div class="col-md-4">
+					<h3 style="color: #87CEEB"><b>CAKE ME OUT</b></h3>
+					<p>Jl. Bumi Berkah 1 No.17 </p>
+					<p><i class="glyphicon glyphicon-earphone"></i> +6281234567890</p>
+					<p><i class="glyphicon glyphicon-envelope"></i> cakemeout@gmail.com</p>
+				</div>
+				<div class="col-md-4">
+					<h5><b>Menu</b></h5>
+					<p><a href=""  style="color: #000">Produk</a></p>
+					<p><a href=""  style="color: #000">About Us</a></p>
+					<p><a href=""  style="color: #000">Hubungi Kami</a></p>
+				</div>
+
+				<div class="col-md-4">
+					
+				</div>
+			</div>
+
+		</div>
+
+		<div class="copy" style="background-color: #87CEEB; padding: 5px; color: #fff; text-align: center;">
+			<span>Copyright&copy; Defti Agita</span>
+		</div>
+	</footer>
+
+</body>
+</html>
